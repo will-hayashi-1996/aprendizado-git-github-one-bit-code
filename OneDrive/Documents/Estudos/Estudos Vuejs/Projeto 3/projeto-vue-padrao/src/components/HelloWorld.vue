86372d15b0ddcd21a3cb3aa0605f5c2a4f9bda41
William Junji Hayashi <hayashi.junji117@gmail.com>
2024-06-08 19:41:41 -0300

    teste

---
+++ OneDrive/Documents/Estudos/Estudos Vuejs/Projeto 3/projeto-vue-padrao/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+ <h1>Componente HelloWorld</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
